1d1034249378ff9d78eae9741d2d59abc8e66b9e
SrNacho <fotosydibujos@hotmail.com>
2021-11-22 06:56:48 -0300

    test

--- training/settings.py
+++ training/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u-8dva#55hd$6r7it%$i5)26+rxrv80u#*s88r=l%4i8f*048='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 AUTHENTICATION_BACKENDS = [
     #'django.contrib.auth.backends.ModelBackend'
